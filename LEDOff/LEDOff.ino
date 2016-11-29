7b8c19a2e4887fde12bd06a227899a315b47e397
LHY <lemonade423@naver.com>
2016-11-29 16:56:29 +0900

    Rearrange file

---
+++ LEDOff/LEDOff.ino
@@ -0,0 +1,22 @@
+#include "LedControl.h"
+LedControl lc = LedControl(12, 11, 10, 1);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  lc.shutdown(0,false);
+  lc.setIntensity(0,8);
+  lc.clearDisplay(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /*
+  for(int row = 0; row < 8; row++) {
+    for(int col = 0; col < 8; col++) {
+      lc.setLed(0, col, row, false);
+      delay(25);
+    }
+  }
+  */
+}
